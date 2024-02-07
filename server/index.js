db6f7f81a4980b4fcf86721fae253476d1b19aab
ash <bittuku14379@gmail.com>
2024-02-08 00:50:46 +0530

    second4

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ app.get('/',async(req,res)=>{
      res.send("server is running from ANIKET SHARMA")
 })
 
+app.get('/use',async(req,res)=>{
+    res.send("yes great ash!!!!!")
+})
 app.listen(process.env.PORT||4000,()=>{
     console.log("server is listening.... ")
 })
