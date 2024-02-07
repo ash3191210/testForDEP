7cf9720e34cba408b5b1f08bc5a9536148641042
ash <bittuku14379@gmail.com>
2024-02-08 01:03:00 +0530

    great

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app=express()
-app.set('view engine','ejs')
+
 app.get('/',async(req,res)=>{
-     res.render('home')
+     res.send('ANIKET SHARMA')
 })
 
 app.get('/use',async(req,res)=>{
