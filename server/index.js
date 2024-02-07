fd7362b843db4fb0356c2edeeec20bbc3d4d7127
ash <bittuku14379@gmail.com>
2024-02-08 01:34:12 +0530

    db

--- server/index.js
+++ server/index.js
@@ -15,8 +15,40 @@ const userschema = new mongoose.Schema({
 const User = mongoose.model('User',userschema)
 
 app.get('/',async(req,res)=>{
+    
      res.send('home')
 })
+
+app.get('/set',async(req,res)=>{
+     try {
+         if(req.query.username && req.query.password){
+             const response = await User.create({
+                 username:req.query.username,
+                 password:req.query.password
+             })
+
+             res.send(`@ ${req.query.username} created successfully!!!!!!`)
+         }
+     } catch (error) {
+        res.send("user is not created")
+     }
+})
+
+
+// app.post('/',async(req,res)=>{
+//     const username = req.params.username;
+//     const password = req.params.password;
+//     console.log(username)
+//     try {
+//         //  const  response = await User.create({
+//         //     username,password
+//         //  })
+
+//         //  res.send(`yes ${username} created successfully`)
+//     } catch (error) {
+//          throw error
+//     }
+// })
 
 app.get('/use',async(req,res)=>{
     res.send("yes great ash!!!!!")
