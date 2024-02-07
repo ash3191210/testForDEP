988112d5a1b1f2abde899a1d739127f611e5f713
ash <bittuku14379@gmail.com>
2024-02-08 01:23:28 +0530

    db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app=express()
-app.set('view engine','ejs')
+
+
+const userschema = new mongoose.Schema({
+     username:{
+        type:String
+     },
+     password:{
+        type:String
+     }
+})
+
+const User = mongoose.model('User',userschema)
+
 app.get('/',async(req,res)=>{
      res.send('home')
 })
@@ -8,6 +21,12 @@ app.get('/',async(req,res)=>{
 app.get('/use',async(req,res)=>{
     res.send("yes great ash!!!!!")
 })
-app.listen(process.env.PORT||4000,()=>{
+app.listen(process.env.PORT||4000,async()=>{
+     try {
+        const conn = await mongoose.connect("mongodb+srv://ash:ash123ishere@cluster0.kfdnrne.mongodb.net/depDB")
+        console.log("db connected....")
+     } catch (error) {
+         console.log("something wrong in db connection")
+     }
     console.log("server is listening.... ")
 })
